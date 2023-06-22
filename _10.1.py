0a6deeb469f79a3cd184bdcb81215d0f002692b3
Smartbrass <136761121+Smartbrass@users.noreply.github.com>
2023-06-23 02:47:32 +0300

    Update _10.1.py

--- _10.1.py
+++ _10.1.py
@@ -5,8 +5,8 @@ OwName = input('Владелец:')
 pets = {PetsName:{"Вид питомца": PetsType, "Возраст питомца": PetsAge, "Имя владельца": OwName}}
 
 if PetsAge % 10 == 1:
-    print('Это', pets[PetsName]["Вид питомца"],'по кличке', '"' + PetsName + '"' + '.', 'Возраст питомца:', PetsAge, 'год' + '.', 'Имя владельца:', OwName, end = '.')
+    print('Это', pets[PetsName]["Вид питомца"],'по кличке', '"' + PetsName + '"' + '.', 'Возраст питомца:', PetsAge, 'год' + '.', 'Имя владельца:', pets[PetsName]["Имя владельца"], end = '.')
 elif PetsAge % 10 > 1 and PetsAge % 10 < 5:
-    print('Это', pets[PetsName]["Вид питомца"],'по кличке', '"' + PetsName + '"' + '.', 'Возраст питомца:', PetsAge, 'года' + '.', 'Имя владельца:', OwName, end = '.')
+    print('Это', pets[PetsName]["Вид питомца"],'по кличке', '"' + PetsName + '"' + '.', 'Возраст питомца:', PetsAge, 'года' + '.', 'Имя владельца:', pets[PetsName]["Имя владельца"], end = '.')
 else:
-    print('Это', pets[PetsName]["Вид питомца"],'по кличке', '"' + PetsName + '"' + '.', 'Возраст питомца:', PetsAge, 'лет' + '.', 'Имя владельца:', OwName, end = '.')
+    print('Это', pets[PetsName]["Вид питомца"],'по кличке', '"' + PetsName + '"' + '.', 'Возраст питомца:', PetsAge, 'лет' + '.', 'Имя владельца:', pets[PetsName]["Имя владельца"], end = '.')
